04b8211cb627806c52aa3748512d842475af2e3c
Jayant Goel <jgoel92@gmail.com>
2021-10-31 12:16:54 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+gsap.to("#witch",{
+	scrollTrigger :{
+		scrub:0.5
+	},
+	x : 1500,
+	y : 500
+});
+gsap.from("#wolf",{
+	scrollTrigger :{
+		scrub:0.5
+	},
+	x : 400
+});
+gsap.from("#castle",{
+	scrollTrigger :{
+		scrub:0.5
+	},
+	x : -100
+});
+gsap.from("#bats",{
+	scrollTrigger :{
+		scrub:0.5
+	},
+	scale : 1.5
+});
